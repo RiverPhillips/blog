2059431bcc625d2c010266fe88204f519407b0d9
River phillips <riverphillips1@gmail.com>
2023-11-04 17:20:00 +0000

    enable sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), sentry({
+		dsn: "https://889623d33b0d38d17cdeea2ab58f45da@o4504895664488448.ingest.sentry.io/4505924641423360",
+		sourceMapsUploadOptions: {
+			project: "blog",
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+		}
+	})]
 });
