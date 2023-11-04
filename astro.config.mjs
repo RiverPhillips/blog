92bd048586330a3a99999aa2eb87e1313bab1460
River phillips <riverphillips1@gmail.com>
2023-11-04 14:54:15 +0000

    Revert "enable analytics"
    This reverts commit 4476ba6425a1346428ec968f1e2737bedbf3b915.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -8,10 +7,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
 });
