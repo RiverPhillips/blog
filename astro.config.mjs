6fa47ef17748fe489be4495ee1a464dbf7d44378
River phillips <riverphillips1@gmail.com>
2023-11-05 16:50:00 +0000

    Use vercel adapater

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.riverphillips.dev/',
 	integrations: [mdx(), sitemap()],
+	output: "server",
+	adapter: vercel()
 });
